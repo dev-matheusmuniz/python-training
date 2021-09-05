6bd14ce7bb9522b0bc7441994cf603824fc7e851
Matheus Muniz <dev.mmuniz@gmail.com>
2021-09-05 02:39:32 -0300

    Difficulty level added for the guessing game

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,15 +1,29 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
-rodada = 1
+numero_secreto = round(random.randrange(1, 101))
+total_de_tentativas = 0
+
+print("Níveis de dificuldade:")
+print("(1) Fácil | (2) Médio | (3) Difícil")
+
+nivel = int(input("Insira a dificuldade desejada: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+print(numero_secreto)
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número entre 1 e 100: ")
-    chute = int(chute_str)
+    chute = int(input("Digite um número entre 1 e 100: "))
     if(chute < 1 or chute > 100):
         print("Seu número está fora do range solicitado!")
         continue

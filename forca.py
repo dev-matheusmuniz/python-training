9492edbf3ae478f1d157096298beecdeabd40f39
Matheus Muniz <dev.mmuniz@gmail.com>
2021-09-05 23:03:00 -0300

    news

--- forca.py
+++ forca.py
@@ -4,7 +4,17 @@ def iniciar_forca():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "porta"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
-    iniciar_forca
+    iniciar_forca()
